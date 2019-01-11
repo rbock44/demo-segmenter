d5289aa23f4f909aaa6319e1eb770611dde9586d
Richard Bock <richard.bock@rbock44.de>
2019-01-11 13:53:12 +0100

    FIX: use old image name for app and remove container_name for schema-registry

--- build.sh
+++ build.sh
@@ -3,12 +3,9 @@
 docker run -it --rm -e HOSTUID=$(id -u) -v $(pwd):/root hseeberger/scala-sbt sh ./build-project.sh
 if [[ $? -eq 0 ]]
 then 
-bash -c "cd driver && docker build -t session-segmenter-driver ."
-bash -c "cd driver && docker tag session-segmenter-driver session-segmenter-driver:1"
-bash -c "cd app && docker build -t session-segmenter-app ."
-bash -c "cd driver && docker tag session-segmenter-app session-segmenter-app:1"
-bash -c "cd reader && docker build -t session-segmenter-reader ."
-bash -c "cd driver && docker tag session-segmenter-reader session-segmenter-reader:1"
+bash -c "cd driver && docker build -t session-segmenter-driver:1 ."
+bash -c "cd app && docker build -t session-segmenter:1 ."
+bash -c "cd reader && docker build -t session-segmenter-reader:1 ."
 else
   exit 1
 fi
